1add2580d3f05c94b7bdcdadd2cadb5485f637ac
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2023-03-23 16:43:03 +0700

    Add files via upload

---
+++ MineSweeperView/MView.java
@@ -0,0 +1,40 @@
+package MineSweeperView;
+
+import MineSweeperController.MinesweeperController;
+
+import java.util.Arrays;
+
+public class MView {
+    private final int N=9;
+    private String[][] vMatrix;
+    private MinesweeperController controller;
+    public MView(MinesweeperController controller){
+        this.controller=controller;
+        vMatrix=new String[N][N];
+        for(int i=0;i<N;i++){
+            Arrays.fill(vMatrix[i],"-");
+        }
+    }
+    public void showMatrix(){
+    	for(int i=-1;i<N;i++) {
+    		System.out.printf("%3d",i);
+    	}
+    	System.out.println();
+        for(int i=0;i<this.N;i++){
+            System.out.printf("%3d",i);
+            for (int j=0;j<this.N;j++){
+                System.out.printf("%3s",vMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public void showValue(int x,int y,int t){
+        this.vMatrix[x][y]=String.valueOf(t);
+    }
+    public void setFlag(int x,int y){
+        this.vMatrix[x][y]="P";
+    }
+    public void unsetFlag(int x,int y){
+        this.vMatrix[x][y]="-";
+    }
+}
