c0e7398835d2c598f8ba79161550ffb92b875cf8
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2023-03-24 22:59:15 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import MineSweeperController.MinesweeperController;
 
+//run program
+
 public class Main {
     public static void main(String[] args) {
         MinesweeperController c=new MinesweeperController();
