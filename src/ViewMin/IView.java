ae426e5ec951e5bb1b1a3d40723c7192b5e33501
Chien Tran <tcc3281@gmail.com>
2023-03-30 20:51:54 +0700

    xoa

--- src/ViewMin/IView.java
+++
@@ -1,9 +0,0 @@
-package ViewMin;
-
-public interface IView {
-	public void opened(int x,int y);//mo o x,y
-	public void setFlag(int x, int y);//cam co o x,y
-	public void unsetFlag(int x, int y);//bo cam co
-	public void win();///thong bao win
-	public void lose();// thua
-}
