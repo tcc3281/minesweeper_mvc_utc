d1bf91ace6c8e2a58fa5ff65f2964438051de84f
chien tran <tcc3281@gmail.com>
2023-04-03 23:43:12 +0700

    dùng control tạo game thay vì dùng trực tiếp gameFrame

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import MineSweeperController.MinesweeperController;
+import Controller.ControlGame;
+import View.GameFrame;
 
 //run program
 
 public class Main {
     public static void main(String[] args) {
-        MinesweeperController c=new MinesweeperController();
+        new ControlGame();
     }
 }
