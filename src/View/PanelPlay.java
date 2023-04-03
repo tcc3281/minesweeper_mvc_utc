0f521205d5637658a0dcaaa78ec8708f49fbef2e
chien tran <tcc3281@gmail.com>
2023-04-03 23:46:42 +0700

    sửa hàm tạo, thêm addMouse

--- src/View/PanelPlay.java
+++ src/View/PanelPlay.java
@@ -5,75 +5,26 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-import Controller.controlgame;
+import Controller.ControlGame;
 
-public class PanelPlay extends JPanel{
-	
+public class PanelPlay extends JPanel {
+
 	private GamePanel gamepanel;
 	private ButtonPlay[][] arrButton;
+
 	public PanelPlay(GamePanel gamepanel) {
 		super();
-		
-		controlgame controlgame = new controlgame(this);
-		this.gamepanel= gamepanel;
 		this.setLayout(new GridLayout(gamepanel.getW(), gamepanel.getH()));
 		this.setBorder(BorderFactory.createLoweredBevelBorder());
 		arrButton = new ButtonPlay[gamepanel.getW()][gamepanel.getH()];
-		for(int i= 0; i< arrButton.length;i++) {
+		for (int i = 0; i < arrButton.length; i++) {
 			for (int j = 0; j < arrButton[i].length; j++) {
-				this.add(arrButton[i][j]= new ButtonPlay());
-				
+				this.add(arrButton[i][j] = new ButtonPlay());
+
 			}
 		}
-		for(int i=0;i<this.arrButton.length;i++) 
-			for(int j=0;j<this.arrButton[i].length;j++) 
-				this.arrButton[i][j].addMouseListener(controlgame);
-		} 
-				//{
-//					
-//					@Override
-//					public void mouseReleased(MouseEvent e) {
-//						// TODO Auto-generated method stub
-//					}
-//					
-//					@Override
-//					public void mousePressed(MouseEvent e) {
-//						// TODO Auto-generated method stub
-//						
-//					}
-//					
-//					@Override
-//					public void mouseExited(MouseEvent e) {
-//						// TODO Auto-generated method stub
-//						
-//					}
-//					
-//					@Override
-//					public void mouseEntered(MouseEvent e) {
-//						// TODO Auto-generated method stub
-//						
-//					}
-//					
-//					@Override
-//					public void mouseClicked(MouseEvent e) {
-//						ButtonPlay[][]arrButton = getArrButton();
-//						for (int i = 0; i < arrButton.length; i++) {
-//							for (int j = 0; j < arrButton[i].length; j++) {
-//								if(e.getButton()== 1 && e.getSource()== arrButton[i][j]) {
-//									//code day
-//									open("5", i, j);
-//								}
-//								else if(e.getButton()==3&& e.getSource()== arrButton[i][j]) {
-//									//code day
-//									//setFlag(i,j);
-//								}
-//							}
-//						}
-//					}
-//				});
-//			}
-//		}
-//	}
+	}
+
 
 	public void open(String number, int x, int y) {
 		this.arrButton[x][y].appearence(number);
@@ -96,5 +47,10 @@ public class PanelPlay extends JPanel{
 	public void setArrButton(ButtonPlay[][] arrButton) {
 		this.arrButton = arrButton;
 	}
-	
+
+	public void addMouse(ControlGame controlGame){
+		for (int i = 0; i < this.arrButton.length; i++)
+			for (int j = 0; j < this.arrButton[i].length; j++)
+				this.arrButton[i][j].addMouseListener(controlGame);
+	}
 }
